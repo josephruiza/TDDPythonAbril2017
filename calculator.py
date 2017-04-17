2aa7e5fdceb67d591b95836dedcb99254738c08c
Alberto Casero <alberto@kasfactory.net>
2017-04-17 21:23:57 +0200

    Escribimos los primeros test de la calculadora testeando el metodo add

---
+++ calculator.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+class Calculator(object):
+
+    def add(self, a, b):
+        return a + b
---
+++ tests.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from calculator import Calculator
+
+
+class TestsCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Se ejecuta siempre antes de cada test
+        """
+        self.calculator = Calculator()
+
+    def tearDown(self):
+        """
+        Se ejecuta siempre después de cada test
+        """
+        self.calculator = None
+
+    def test_add_1_2(self):
+        result = self.calculator.add(1, 2)  # 1 + 2 =  3
+        self.assertEqual(result, 3)
+
+    def test_add_5_5(self):
+        result = self.calculator.add(5, 5)  # 5 + 5 = 10
+        self.assertEqual(result, 10)
+
+
+# comprueba si se está ejecutando directamente este archivo con el comando: python tests.py y, de ser así, arranca los test
+if __name__ == "__main__":
+    unittest.main()
