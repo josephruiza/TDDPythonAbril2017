bd3c286e3a162e07dad6777d17e92cc3aac792a7
Alberto Casero <alberto@kasfactory.net>
2017-04-17 21:31:07 +0200

    Test de la propiedad conmutativa

--- tests.py
+++ tests.py
@@ -26,6 +26,12 @@ class TestsCalculator(unittest.TestCase):
         result = self.calculator.add(5, 5)  # 5 + 5 = 10
         self.assertEqual(result, 10)
 
+    def test_commutative_property(self):
+        result_a = self.calculator.add(3, 4)
+        result_b = self.calculator.add(4, 3)
+        self.assertEqual(result_a, result_b)
+
+
 
 # comprueba si se está ejecutando directamente este archivo con el comando: python tests.py y, de ser así, arranca los test
 if __name__ == "__main__":
